f225440806fb2bdf75e7688cb0f3bc280b043d87
CaueAlvesSilva <cauesilva@MacBook-Pro-de-Caue.local>
2015-05-02 16:08:59 -0300

    TableView com CoreData
    - TableView buscando repositorios do CoreData (vazio)
    - Imagens na tela de login
    - Inclusão de Marks no código

--- DesafioGit/DesafioGit/RepositoryTableViewController.swift
+++ DesafioGit/DesafioGit/RepositoryTableViewController.swift
@@ -8,81 +8,78 @@
 
 import UIKit
 
-class RepositoryTableViewController: UITableViewController {
+class RepositoryTableViewController: UITableViewController, UITableViewDataSource {
 
-    override func viewDidLoad() {
+    
+    // MARK: - Properties
+    //================================================================================
+    lazy var repositories:Array<Repository> =
+    {
+        return RepositoriesManager.sharedInstance.findRepositories()
+    }()
+    //================================================================================
+    
+    
+    
+    // MARK: - VC's Life Cycle
+    //================================================================================
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Potentially incomplete method implementation.
-        // Return the number of sections.
+    
+    override func viewWillAppear(animated: Bool)
+    {
+        self.navigationItem.title = "GitHub"
+        repositories = RepositoriesManager.sharedInstance.findRepositories()
+        self.tableView.reloadData()
+    }
+    //================================================================================
+    
+    
+    
+    // MARK: - TableView Properties
+    //================================================================================
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int
+    {
         return 1
     }
+    
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String?
+    {
+        return "Repositories"
+    }
+    
 
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete method implementation.
-        // Return the number of rows in the section.
-        return 1
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return repositories.count
     }
 
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let item:Repository = repositories[indexPath.row]
+        
         let cell = tableView.dequeueReusableCellWithIdentifier("showDetail", forIndexPath: indexPath) as! UITableViewCell
+        
+        cell.textLabel?.text = item.name
 
         return cell
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
+    //================================================================================
+    
+    
+    
+    // MARK: - Prepare for segue
+    //================================================================================
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?)
+    {
+        if let destino = segue.destinationViewController as? DetailViewController
+        {
+            destino.repositoryName = repositories[tableView.indexPathForSelectedRow()!.row].name
+        }
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    //================================================================================
 }
